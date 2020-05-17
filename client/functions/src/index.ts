cebafbd19628f9d7a993b9b3cc04a6d47eedd401
Franjo Pozaic <f.pozaic@reply.de>
2020-05-17 21:47:40 +0200

    Random function works

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -1,8 +1,34 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as R from 'ramda';
+// import {QuerySnapshot} from '@google-cloud/firestore'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({ credential: admin.credential.applicationDefault() });
+
+export const randomSong = functions.https.onRequest((request, response) => {
+  admin
+    .firestore()
+    .collection('journal')
+    .get()
+    .then((items) => {
+      const allDocs = items.docs.map((d) => d.data());
+      console.log('Docs', allDocs);
+
+      const song = getRandomSong(allDocs);
+      console.log('Random', song);
+      response.send(song);
+    })
+    .catch((_) => response.send([0]));
+});
+
+
+
+const getRandomSong = R.pipe(
+  R.map((item: any) => item.songs || []),
+  R.flatten,
+  R.map(R.pick(['artist', 'title', 'videoUrl'])),
+  R.pick([`${Math.floor(Math.random() * 2)}`])
+)
